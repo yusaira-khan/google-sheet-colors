8bec0e0faf8e62f612e18989cf3d1cc2f9ff4429
Yusaira Khan <hello@yusaira.ca>
2025-12-04 12:03:33 -0500

    Add color constants
    Added a comprehensive color palette with constants for various colors.

---
+++ colors.js
@@ -0,0 +1,163 @@
+const COLOR_BLACK = "#000000"
+const COLOR_DARK_GRAY_4 = "#434343"
+const COLOR_DARK_GRAY_3 = "#666666"
+const COLOR_DARK_GRAY_2 = "#999999"
+const COLOR_DARK_GRAY_1 = "#b7b7b7"
+const COLOR_GRAY = "#cccccc"
+const COLOR_LIGHT_GRAY_1 = "#d9d9d9"
+const COLOR_LIGHT_GRAY_2 = "#efefef"
+const COLOR_LIGHT_GRAY_3 = "#f3f3f3"
+const COLOR_WHITE = "#ffffff"
+const COLOR_RED_BERRY = "#980000"
+const COLOR_LIGHT_RED_BERRY_3 = "#e6b8af"
+const COLOR_LIGHT_RED_BERRY_2 = "#dd7e6b"
+const COLOR_LIGHT_RED_BERRY_1 = "#cc4125"
+const COLOR_DARK_RED_BERRY_1 = "#a61c00"
+const COLOR_DARK_RED_BERRY_2 = "#85200c"
+const COLOR_DARK_RED_BERRY_3 = "#5b0f00"
+const COLOR_RED = "#ff0000"
+const COLOR_LIGHT_RED_3 = "#f4cccc"
+const COLOR_LIGHT_RED_2 = "#ea9999"
+const COLOR_LIGHT_RED_1 = "#e06666"
+const COLOR_DARK_RED_1 = "#cc0000"
+const COLOR_DARK_RED_2 = "#990000"
+const COLOR_DARK_RED_3 = "#660000"
+const COLOR_ORANGE = "#ff9900"
+const COLOR_LIGHT_ORANGE_3 = "#fce5cd"
+const COLOR_LIGHT_ORANGE_2 = "#f9cb9c"
+const COLOR_LIGHT_ORANGE_1 = "#f6b26b"
+const COLOR_DARK_ORANGE_1 = "#e69138"
+const COLOR_DARK_ORANGE_2 = "#b45f06"
+const COLOR_DARK_ORANGE_3 = "#783f04"
+const COLOR_YELLOW = "#ffff00"
+const COLOR_LIGHT_YELLOW_3 = "#fff2cc"
+const COLOR_LIGHT_YELLOW_2 = "#ffe599"
+const COLOR_LIGHT_YELLOW_1 = "#ffd966"
+const COLOR_DARK_YELLOW_1 = "#f1c232"
+const COLOR_DARK_YELLOW_2 = "#bf9000"
+const COLOR_DARK_YELLOW_3 = "#7f6000"
+const COLOR_GREEN = "#00ff00"
+const COLOR_LIGHT_GREEN_3 = "#d9ead3"
+const COLOR_LIGHT_GREEN_2 = "#b6d7a8"
+const COLOR_LIGHT_GREEN_1 = "#93c47d"
+const COLOR_DARK_GREEN_1 = "#6aa84f"
+const COLOR_DARK_GREEN_2 = "#38761d"
+const COLOR_DARK_GREEN_3 = "#274e13"
+const COLOR_CYAN = "#00ffff"
+const COLOR_LIGHT_CYAN_3 = "#d0e0e3"
+const COLOR_LIGHT_CYAN_2 = "#a2c4c9"
+const COLOR_LIGHT_CYAN_1 = "#76a5af"
+const COLOR_DARK_CYAN_1 = "#45818e"
+const COLOR_DARK_CYAN_2 = "#134f5c"
+const COLOR_DARK_CYAN_3 = "#0c343d"
+const COLOR_CORN_FLOWER_BLUE = "#4a86e8"
+const COLOR_LIGHT_CORN_FLOWER_BLUE_3 = "#c9daf8"
+const COLOR_LIGHT_CORN_FLOWER_BLUE_2 = "#a4c2f4"
+const COLOR_LIGHT_CORN_FLOWER_BLUE_1 = "#6d9eeb"
+const COLOR_DARK_CORN_FLOWER_BLUE_1 = "#3c78d8"
+const COLOR_DARK_CORN_FLOWER_BLUE_2 = "#1155cc"
+const COLOR_DARK_CORN_FLOWER_BLUE_3 = "#1c4587"
+const COLOR_BLUE = "#0000ff"
+const COLOR_LIGHT_BLUE_3 = "#cfe2f3"
+const COLOR_LIGHT_BLUE_2 = "#9fc5e8"
+const COLOR_LIGHT_BLUE_1 = "#6fa8dc"
+const COLOR_DARK_BLUE_1 = "#3d85c6"
+const COLOR_DARK_BLUE_2 = "#0b5394"
+const COLOR_DARK_BLUE_3 = "#073763"
+const COLOR_PURPLE = "#9900ff"
+const COLOR_LIGHT_PURPLE_3 = "#d9d2e9"
+const COLOR_LIGHT_PURPLE_2 = "#b4a7d6"
+const COLOR_LIGHT_PURPLE_1 = "#8e7cc3"
+const COLOR_DARK_PURPLE_1 = "#674ea7"
+const COLOR_DARK_PURPLE_2 = "#351c75"
+const COLOR_DARK_PURPLE_3 = "#20124d"
+const COLOR_MAGENTA = "#ff00ff"
+const COLOR_LIGHT_MAGENTA_3 = "#ead1dc"
+const COLOR_LIGHT_MAGENTA_2 = "#d5a6bd"
+const COLOR_LIGHT_MAGENTA_1 = "#c27ba0"
+const COLOR_DARK_MAGENTA_1 = "#a64d79"
+const COLOR_DARK_MAGENTA_2 = "#741b47"
+const COLOR_DARK_MAGENTA_3 = "#4c1130"               
+
+const COLORS={
+  "black": "#000000",
+  "dark_gray_4": "#434343",
+  "dark_gray_3": "#666666",
+  "dark_gray_2": "#999999",
+  "dark_gray_1": "#b7b7b7",
+  "gray": "#cccccc",
+  "light_gray_1": "#d9d9d9",
+  "light_gray_2": "#efefef",
+  "light_gray_3": "#f3f3f3",
+  "white": "#ffffff",
+  "red_berry": "#980000",
+  "light_red_berry_3": "#e6b8af",
+  "light_red_berry_2": "#dd7e6b",
+  "light_red_berry_1": "#cc4125",
+  "dark_red_berry_1": "#a61c00",
+  "dark_red_berry_2": "#85200c",
+  "dark_red_berry_3": "#5b0f00",
+  "red": "#ff0000",
+  "light_red_3": "#f4cccc",
+  "light_red_2": "#ea9999",
+  "light_red_1": "#e06666",
+  "dark_red_1": "#cc0000",
+  "dark_red_2": "#990000",
+  "dark_red_3": "#660000",
+  "orange": "#ff9900",
+  "light_orange_3": "#fce5cd",
+  "light_orange_2": "#f9cb9c",
+  "light_orange_1": "#f6b26b",
+  "dark_orange_1": "#e69138",
+  "dark_orange_2": "#b45f06",
+  "dark_orange_3": "#783f04",
+  "yellow": "#ffff00",
+  "light_yellow_3": "#fff2cc",
+  "light_yellow_2": "#ffe599",
+  "light_yellow_1": "#ffd966",
+  "dark_yellow_1": "#f1c232",
+  "dark_yellow_2": "#bf9000",
+  "dark_yellow_3": "#7f6000",
+  "green": "#00ff00",
+  "light_green_3": "#d9ead3",
+  "light_green_2": "#b6d7a8",
+  "light_green_1": "#93c47d",
+  "dark_green_1": "#6aa84f",
+  "dark_green_2": "#38761d",
+  "dark_green_3": "#274e13",
+  "cyan": "#00ffff",
+  "light_cyan_3": "#d0e0e3",
+  "light_cyan_2": "#a2c4c9",
+  "light_cyan_1": "#76a5af",
+  "dark_cyan_1": "#45818e",
+  "dark_cyan_2": "#134f5c",
+  "dark_cyan_3": "#0c343d",
+  "corn_flower_blue": "#4a86e8",
+  "light_corn_flower_blue_3": "#c9daf8",
+  "light_corn_flower_blue_2": "#a4c2f4",
+  "light_corn_flower_blue_1": "#6d9eeb",
+  "dark_corn_flower_blue_1": "#3c78d8",
+  "dark_corn_flower_blue_2": "#1155cc",
+  "dark_corn_flower_blue_3": "#1c4587",
+  "blue": "#0000ff",
+  "light_blue_3": "#cfe2f3",
+  "light_blue_2": "#9fc5e8",
+  "light_blue_1": "#6fa8dc",
+  "dark_blue_1": "#3d85c6",
+  "dark_blue_2": "#0b5394",
+  "dark_blue_3": "#073763",
+  "purple": "#9900ff",
+  "light_purple_3": "#d9d2e9",
+  "light_purple_2": "#b4a7d6",
+  "light_purple_1": "#8e7cc3",
+  "dark_purple_1": "#674ea7",
+  "dark_purple_2": "#351c75",
+  "dark_purple_3": "#20124d",
+  "magenta": "#ff00ff",
+  "light_magenta_3": "#ead1dc",
+  "light_magenta_2": "#d5a6bd",
+  "light_magenta_1": "#c27ba0",
+  "dark_magenta_1": "#a64d79",
+  "dark_magenta_2": "#741b47",
+  "dark_magenta_3": "#4c1130",
+}
